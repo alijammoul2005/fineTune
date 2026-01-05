d494f7dca61699e5f8aa23be8a14483208fe3819
alijammoul2005 <89688461+alijammoul2005@users.noreply.github.com>
2026-01-05 12:19:31 +0200

    documentation

--- starty.py
+++ starty.py
@@ -1,5 +1,5 @@
 from huggingface_hub import login
-login("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")
+login("")
 
 #=========================================================================================================
 
